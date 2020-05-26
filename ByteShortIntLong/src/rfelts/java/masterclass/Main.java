806a81f2ed82214a98a97762449c930f7a4e7751
rfelts <rfelts@adobe.com>
2020-05-25 23:39:41 -0700

    Int min max

---
+++ ByteShortIntLong/src/rfelts/java/masterclass/Main.java
@@ -0,0 +1,15 @@
+package rfelts.java.masterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Min value = " + myMinIntValue);
+        System.out.println("Integer Max value = " + myMaxIntValue);
+        System.out.println("Integer Max value = " + (myMaxIntValue + 1));
+        System.out.println("Integer Min value = " + (myMinIntValue - 1));
+
+    }
+}
