b754b26379c948949536eae638d144e9577f8e93
rfelts <rfelts@adobe.com>
2020-05-25 23:43:10 -0700

    Byte Min Max

--- ByteShortIntLong/src/rfelts/java/masterclass/Main.java
+++ ByteShortIntLong/src/rfelts/java/masterclass/Main.java
@@ -11,5 +11,10 @@ public class Main {
         System.out.println("Integer Max value = " + (myMaxIntValue + 1));
         System.out.println("Integer Min value = " + (myMinIntValue - 1));
 
+        byte myByteMinValue = Byte.MIN_VALUE;
+        byte myByteMaxValue = Byte.MAX_VALUE;
+        System.out.println("Byte Min Value " + myByteMinValue);
+        System.out.println("Byte Max Value " + myByteMaxValue);
+
     }
 }
