9c1aaa627febbd61bbd8b8d082b5c7f7bcc42a81
rfelts <rfelts@adobe.com>
2020-05-25 23:17:29 -0700

    Added int with print statment

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String [] args) {
         System.out.println("Hello Russell");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
