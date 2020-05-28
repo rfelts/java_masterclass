2717bb2dd9a421b1bd6db7006c552effc1ce3973
rfelts <rfelts@adobe.com>
2020-05-27 23:26:13 -0700

    Char and boolean

---
+++ CharAndBoolean/src/rfelts/java/masterclass/Main.java
@@ -0,0 +1,15 @@
+package rfelts.java.masterclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char myChar = 'c';
+        System.out.println(myChar);
+        char unicodeChar = '\u00A9';
+        System.out.println(unicodeChar);
+
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+    }
+}
